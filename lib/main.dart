8d9bac0bc3ac74770b6efa3f06712959a386677e
minchurl <minchurl001@gmail.com>
2024-07-10 19:52:15 +0900

    final!!

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Photo Table',
       home: Container(
         decoration: BoxDecoration(
